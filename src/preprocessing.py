45f7226714b618ca465e2948c120fa8977fb3d14
mehdimunim <mehdi.munim@gmail.com>
2024-12-03 14:45:09 +0100

    Update working API

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -7,8 +7,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from gensim.models import Word2Vec, Doc2Vec
 from gensim.models.doc2vec import TaggedDocument
 import numpy as np
-
-
+import pickle
 
 def clean_tweet(tweet):
     """
@@ -45,7 +44,7 @@ def clean_tweet(tweet):
 
 def vectorize_tfidf(X_train, X_test):
     """
-    Vectorise les données textuelles avec TF-IDF.
+    Vectorise les données textuelles avec TF-IDF et sauvegarde le vectorizer.
 
     Args:
         X_train : Les données d'entraînement.
@@ -58,12 +57,17 @@ def vectorize_tfidf(X_train, X_test):
     vectorizer_tfidf = TfidfVectorizer()
     X_train_tfidf = vectorizer_tfidf.fit_transform(X_train)
     X_test_tfidf = vectorizer_tfidf.transform(X_test)
+
+    # Sauvegarder le vectorizer TF-IDF
+    with open('../models/tfidf_vectorizer.pkl', 'wb') as f:
+        pickle.dump(vectorizer_tfidf, f)
+
     return X_train_tfidf, X_test_tfidf
 
 
 def vectorize_word2vec(X_train, X_test):
     """
-    Vectorise les données textuelles avec Word2Vec.
+    Vectorise les données textuelles avec Word2Vec et sauvegarde le modèle.
 
     Args:
         X_train : Les données d'entraînement.
@@ -89,12 +93,15 @@ def vectorize_word2vec(X_train, X_test):
     X_train_w2v = np.array([vectorize_tweet_w2v(tweet) for tweet in X_train])
     X_test_w2v = np.array([vectorize_tweet_w2v(tweet) for tweet in X_test])
 
+    # Sauvegarder le modèle Word2Vec
+    model_w2v.save('../models/word2vec_model.pkl')
+
     return X_train_w2v, X_test_w2v
 
 
 def vectorize_doc2vec(X_train, X_test):
     """
-    Vectorise les données textuelles avec Doc2Vec.
+    Vectorise les données textuelles avec Doc2Vec et sauvegarde le modèle.
 
     Args:
         X_train : Les données d'entraînement.
@@ -118,17 +125,22 @@ def vectorize_doc2vec(X_train, X_test):
     X_train_d2v = np.array([vectorize_tweet_d2v(tweet) for tweet in X_train])
     X_test_d2v = np.array([vectorize_tweet_d2v(tweet) for tweet in X_test])
 
+    # Sauvegarder le modèle Doc2Vec
+    model_d2v.save('../models/doc2vec_model.pkl')
+
     return X_train_d2v, X_test_d2v
 
-def reduce_dimensionality(X_train, X_test, y_train, k=1000):
+def reduce_dimensionality(X_train, X_test, y_train, k=1000, nom="tfidf"):
     """
-    Réduit la dimensionnalité des données en utilisant SelectKBest avec le test ANOVA F-value.
+    Réduit la dimensionnalité des données en utilisant SelectKBest avec le test ANOVA F-value
+    et sauvegarde le sélecteur de features.
 
     Args:
         X_train : Les données d'entraînement.
         X_test : Les données de test.
         y_train : Les étiquettes d'entraînement.
         k : Le nombre de features à sélectionner.
+        nom : Le nom de la méthode de vectorisation (tfidf, word2vec, doc2vec, etc.).
 
     Returns:
         X_train_reduced, X_test_reduced : Les données avec une dimensionnalité réduite.
@@ -137,4 +149,9 @@ def reduce_dimensionality(X_train, X_test, y_train, k=1000):
     selector = SelectKBest(f_classif, k=k)
     X_train_reduced = selector.fit_transform(X_train, y_train)
     X_test_reduced = selector.transform(X_test)
+
+    # Sauvegarder le sélecteur de features avec un nom spécifique à la méthode
+    with open(f'../models/feature_selector_{nom}.pkl', 'wb') as f:
+        pickle.dump(selector, f)
+
     return X_train_reduced, X_test_reduced

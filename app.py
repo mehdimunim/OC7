872c01590400decfc740367e76797c94536232b1
mehdimunim <mehdi.munim@gmail.com>
2024-12-27 21:26:59 +0100

    Revert change in app.py

--- app.py
+++ app.py
@@ -2,45 +2,9 @@ from flask import Flask, request, jsonify
 import pickle
 import sys
 
-import nltk
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
-import re
-
-def clean_tweet(tweet):
-    """
-    Nettoie le texte d'un tweet en supprimant les mentions, les hashtags,
-    les liens, les caractères spéciaux, la ponctuation, et en convertissant
-    le texte en minuscules.
-
-    Args:
-        tweet : Le texte du tweet à nettoyer.
-
-    Returns:
-        Le texte du tweet nettoyé.
-    """
-
-    # Supprimer les mentions, les hashtags et les liens
-    tweet = re.sub(r"@[A-Za-z0-9_]+|#[A-Za-z0-9_]+|http\S+", "", tweet)
-
-    # Supprimer les caractères spéciaux et la ponctuation
-    tweet = re.sub(r"[^a-zA-Z ]", "", tweet)
-
-    # Convertir le texte en minuscules
-    tweet = tweet.lower()
-
-    # Supprimer les mots vides
-    stop_words = set(stopwords.words('english'))
-    tweet = " ".join([word for word in tweet.split() if word not in stop_words])
-
-    # Lemmatiser les mots
-    lemmatizer = WordNetLemmatizer()
-    tweet = " ".join([lemmatizer.lemmatize(word) for word in tweet.split()])
-
-    return tweet
-
-
-
+# Ajouter le chemin du dossier src pour importer les modules
+sys.path.append('src')
+from clean_tweet import clean_tweet
 
 
 app = Flask(__name__)

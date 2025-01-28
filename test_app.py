62af42380f0d4d975cd16c7eeb88cb125f07f213
EC2 Default User <ec2-user@ip-172-31-84-52.ec2.internal>
2025-01-28 12:42:53 +0000

    Update tests

--- test_app.py
+++ test_app.py
@@ -6,7 +6,7 @@ def client():
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
-
+# test
 def test_predict_positive_sentiment(client):
     response = client.post('/predict', data={'tweet': 'This is a great movie!'})
     assert response.status_code == 200
@@ -16,4 +16,5 @@ def test_predict_negative_sentiment(client):
     response = client.post('/predict', data={'tweet': 'This movie is terrible.'})
     assert response.status_code == 200
     assert response.json['sentiment'] == 'negatif'
+
 

ff570c9e6c282a6343f5a4a8bbb6fb8fae2b1cc8
mehdimunim <mehdi.munim@gmail.com>
2025-02-08 15:52:19 +0100

    Refactoring

--- test_app.py
+++ test_app.py
@@ -1,17 +1,19 @@
 import pytest
-from app import app  # Importer ton application Flask
+from app import app  
 
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
-# test 
+        
+# Test sentiment positif
 def test_predict_positive_sentiment(client):
     response = client.post('/predict', data={'tweet': 'This is a great movie!'})
     assert response.status_code == 200
     assert response.json['sentiment'] == 'positif'
 
+# Test sentiment négatif
 def test_predict_negative_sentiment(client):
     response = client.post('/predict', data={'tweet': 'This movie is terrible.'})
     assert response.status_code == 200
